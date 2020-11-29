2f1faab78af53ec9eb124fb7cb1660a29a68082b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-29 20:05:07 -0300

    Injeta notificador de e-mail por meio de bean customizada, com @Configuration e @Bean

--- src/main/java/com/danielarrais/algafood/di/notificacao/Notificador.java
+++ src/main/java/com/danielarrais/algafood/di/notificacao/Notificador.java
@@ -2,7 +2,11 @@ package com.danielarrais.algafood.di.notificacao;
 
 import org.springframework.stereotype.Component;
 
+import com.danielarrais.algafood.di.modelo.Cliente;
+
 @Component
 public interface Notificador {
+
+	void notificar(Cliente cliente, String string);
 
 }
--- src/main/java/com/danielarrais/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/danielarrais/algafood/di/notificacao/NotificadorEmail.java
@@ -4,9 +4,41 @@ import org.springframework.stereotype.Component;
 
 import com.danielarrais.algafood.di.modelo.Cliente;
 
-@Component
 public class NotificadorEmail implements Notificador {
+	
+	private boolean caixaAlta;	
+	private String hostServidorSMTP;
+	
+	public NotificadorEmail(String hostServidorSMTP) {
+		this.hostServidorSMTP = hostServidorSMTP;
+	}
+
+	
 	public void notificar(Cliente cliente, String mensagem) {
+		if(this.caixaAlta) {
+			mensagem = mensagem.toUpperCase();
+		}
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+
+
+	public boolean isCaixaAlta() {
+		return caixaAlta;
+	}
+
+
+	public String getHostServidorSMTP() {
+		return hostServidorSMTP;
+	}
+
+
+	public void setCaixaAlta(boolean caixaAlta) {
+		this.caixaAlta = caixaAlta;
+	}
+
+
+	public void setHostServidorSMTP(String hostServidorSMTP) {
+		this.hostServidorSMTP = hostServidorSMTP;
 	}
 }
