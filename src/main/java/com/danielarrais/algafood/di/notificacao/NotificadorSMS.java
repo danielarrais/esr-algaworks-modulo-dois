3277b6d70b10ef4b5be4c0c9f17dc51375d2ba56
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-29 21:01:15 -0300

    Cria novo notificador para gerar ambiguidade de beans

---
+++ src/main/java/com/danielarrais/algafood/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,34 @@
+package com.danielarrais.algafood.di.notificacao;
+
+
+import org.springframework.stereotype.Component;
+
+import com.danielarrais.algafood.di.modelo.Cliente;
+
+@Component
+public class NotificadorSMS implements Notificador {
+	
+	private boolean caixaAlta;	
+	
+	public NotificadorSMS() {
+	}
+
+	
+	public void notificar(Cliente cliente, String mensagem) {
+		if(this.caixaAlta) {
+			mensagem = mensagem.toUpperCase();
+		}
+		
+		System.out.printf("Notificando %s através do telefone %s: %s\n", cliente.getNome(), cliente.getTelefone(), mensagem);
+	}
+
+
+	public boolean isCaixaAlta() {
+		return caixaAlta;
+	}
+
+
+	public void setCaixaAlta(boolean caixaAlta) {
+		this.caixaAlta = caixaAlta;
+	}
+}
