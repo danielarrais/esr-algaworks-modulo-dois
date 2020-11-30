0a5307fafb23464b3f54132d102f490b0c96560b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-29 23:16:06 -0300

    Cria classe de propriedades que ficam vinculadas ao arquivo .properties do sistema

---
+++ src/main/java/com/danielarrais/algafood/di/notificacao/NotificadorEmailProperties.java
@@ -0,0 +1,29 @@
+package com.danielarrais.algafood.di.notificacao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email")
+public class NotificadorEmailProperties {
+
+	private String hostServidor;
+	private Integer portaServidor;
+
+	public String getHostServidor() {
+		return hostServidor;
+	}
+
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
+}
