e994fd762bdb8ef7d9793237849d21dd1f9c8e00
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-29 21:24:39 -0300

    Cria @Qualifier customizado

---
+++ src/main/java/com/danielarrais/algafood/di/notificacao/TipoDoNotificador.java
@@ -0,0 +1,12 @@
+package com.danielarrais.algafood.di.notificacao;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TipoDoNotificador {
+	TipoNotificacao value();
+}
