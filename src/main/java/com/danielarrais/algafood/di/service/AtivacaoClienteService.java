6e1ed64cf8b572bb1c44b654fa9ac823742f2e5a
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-29 21:03:43 -0300

    Resolve ambiguidade de beans com injeção de lista

--- src/main/java/com/danielarrais/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/danielarrais/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,7 @@
 package com.danielarrais.algafood.di.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +12,7 @@ import com.danielarrais.algafood.di.notificacao.Notificador;
 public class AtivacaoClienteService {
 	
 	@Autowired(required = false)
-	private Notificador notificador;
+	private List<Notificador> notificadores;
 	
 	public AtivacaoClienteService() {
 	}
@@ -18,11 +20,8 @@ public class AtivacaoClienteService {
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
-		if(notificador == null) {
-			System.out.println("Não há um notificador disponível!");
-		} else {
-
+		notificadores.stream().forEach(notificador -> {
 			notificador.notificar(cliente, "Seu cadastro no sistema agora está ativo!");
-		}
+		});
 	}
 }
